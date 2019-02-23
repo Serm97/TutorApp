bfea6fcc6f38a2a6fb91e641be5e74f56e2baa8e
jecpulido <jorgepulido95@hotmail.com>
2019-02-23 17:33:12 -0500

    Corrections
    I add corrections of problems generated by merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
 import { UserServiceService } from './services/user-service.service';
 import { ModalPageModule } from './modal/viewTutor/modal.module';
 
