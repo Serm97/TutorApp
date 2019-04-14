867c77e372f7c425a623ce2966c61f342ed730a2
jecpulido <jorgepulido95@hotmail.com>
2019-04-13 19:49:09 -0500

    I added seeker module
    I added seeker module to find tutors

---
+++ TeachApp/app/src/main/java/com/teachapp/teachapp/SeekerActivity.java
@@ -0,0 +1,13 @@
+package com.teachapp.teachapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeekerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seeker);
+    }
+}
